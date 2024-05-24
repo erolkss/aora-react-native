886645e5cbf6100cd7245fa4b10d41b2e6986033
Lucas Lima <lucaserolima@gmail.com>
2024-05-24 16:15:54 -0400

    feat: Created a Video Card in home page a create a new lib to data of AppWrite.

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,26 +1,38 @@
-import { View, FlatList, Text, Image, RefreshControl } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import useAppWrite from "../../lib/useAppWrite";
+import { getAllPosts } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppWrite(getAllPosts);
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = async () => {
     setRefreshing(true);
     // re call videos -> if any new videos appeard
+    await refetch();  
     setRefreshing(false);
   };
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard video={item}/>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
