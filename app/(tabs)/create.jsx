3e5a50c102bacfef9bba0c23019c4c820cb1ce60
Lucas Lima <lucaserolima@gmail.com>
2024-05-28 18:06:55 -0400

    feat: Created the first part, which is just the 'Create' screen

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,12 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import FormField from "../../components/FormField";
+import { ResizeMode, Video } from "expo-av";
+import { icons } from "../../constants";
+import CustomButton from "../../components/CustomButton";
 
 const Create = () => {
-  return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  )
-}
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+  const submit = () => {
 
-export default Create
+  }
+  return (
+    <SafeAreaView className="bg-primary h-full pt-10">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a catch title..."
+          handleChangeText={(e) => ({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Upload Video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-gray-300 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Thumbnail Image
+          </Text>
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-gray-300 rounded-2xl justify-center items-center border-2 border-black-200  flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-5 h-5"
+                />
+                <Text className="text-sm tex-gray-100 font-pmedium">
+                  Choose a file
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+        <FormField
+          title="AI Prompt"
+          value={form.prompt}
+          placeholder="The prompt  you used to create this video"
+          handleChangeText={(e) => ({ ...form, prompt: e })}
+          otherStyles="mt-7"
+        />
+        <CustomButton
+          title="Submit & Publish"
+          handlePress={submit}
+          containerStyles="mt-7"
+          isLoading={uploading}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Create;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
         primary: '#161622',
         secondary: {
           DEFAULT: '#FF9C01',
-          100: 'FF9001',
-          200: 'FF8E01'
+          100: '#FF9001',
+          200: '#FF8E01'
         },
         black: {
           DEFAULT: '#000',
-          100: '1E1E2D',
-          200: '232533'
+          100: '#393050',
+          200: '#232533'
         },
         gray: {
           100: '#CDCDE0'
